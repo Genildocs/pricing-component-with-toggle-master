3d54844ca2a1c841e1a9adeb887c02d3b9d73042
genildocs <genildocs@gmail.com>
2024-06-07 21:10:29 -0300

    commit

--- script/index.js
+++ script/index.js
@@ -1,21 +1,25 @@
-const btnSelect = document.getElementById("btnSelect");
-const select = document.getElementById("select");
-const cardOne = document.querySelector(".card-1");
-const cardTwo = document.querySelector(".card-2");
+const btnSelect = document.getElementById('btnSelect');
+const select = document.getElementById('select');
+const cardOne = document.querySelectorAll('.card-1');
+const cardTwo = document.querySelectorAll('.card-2');
 
 //Functions
 const toogle = () => {
-  cardOne.classList.toggle("hidden");
-  cardTwo.classList.toggle("!flex");
+  cardOne.forEach((card) => {
+    card.classList.toggle('isHidden');
+  });
+  cardTwo.forEach((card) => {
+    card.classList.toggle('isHidden');
+  });
 };
 const selectPlan = () => {
-  if (select.style.transform === "translateX(15px)") {
-    select.style.transform = "translateX(0px)";
+  if (select.style.transform === 'translateX(15px)') {
+    select.style.transform = 'translateX(0px)';
   } else {
-    select.style.transform = "translateX(15px)";
+    select.style.transform = 'translateX(15px)';
   }
 
   toogle();
 };
 //Events
-btnSelect.addEventListener("click", selectPlan);
+btnSelect.addEventListener('click', selectPlan);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': '#a3a8f0',
+        'dark-blue': '#696fdd',
+      },
+      boxShadow: {
+        card: '0px 0px 19px 2px rgba(163,168,240,1)',
+      },
+    },
   },
   plugins: [],
-}
-
+};
